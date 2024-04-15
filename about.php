a524a0de79a90ac3d574ae7ce0412caf7351f08f
mukfat-technologies-smc-ltd <156694354+mukfat-technologies-smc-ltd@users.noreply.github.com>
2024-04-15 03:28:12 -0700

    u

--- about.php
+++ about.php
@@ -29,7 +29,7 @@
 
         <div class="row gy-4" data-aos="fade-up">
           <div class="col-lg-4">
-            <img src="assets/img/6.jpg" class="img-fluid" alt="">
+            <img style="height: 500px;" src="files/aboutus/1.webp" class="img-fluid" alt="">
           </div>
           <div class="col-lg-8">
             <div class="content ps-lg-5">
