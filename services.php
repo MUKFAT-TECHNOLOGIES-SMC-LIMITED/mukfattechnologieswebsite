562f580a44673f620c9d3bd9b570770b1914c8e5
mukfat-technologies-smc-ltd <156694354+mukfat-technologies-smc-ltd@users.noreply.github.com>
2024-04-15 02:42:48 -0700

    u

--- services.php
+++ services.php
@@ -11,7 +11,7 @@
   <main id="main">
 
     <!-- ======= Breadcrumbs ======= -->
-    <div class="breadcrumbs d-flex align-items-center" style="background-image: url('assets/img/services-header.webp');">
+    <div class="breadcrumbs d-flex align-items-center" style="background-image: url('files/otherheader/1.webp');">
       <div class="container position-relative d-flex flex-column align-items-center">
 
         <h2>Services</h2>
