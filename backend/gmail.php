60daa24e05968018fa97b7680331ea74ca661eef
mukfat-technologies-smc-ltd <156694354+mukfat-technologies-smc-ltd@users.noreply.github.com>
2024-04-25 13:18:13 -0700

    U

--- backend/gmail.php
+++ backend/gmail.php
@@ -12,7 +12,7 @@ include 'database.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 // Function to send email
-function sendConfirmationEmail($name, $email, $subject, $message,$caller) {
+function sendConfirmationEmail($name, $email, $service, $message,$contact,$caller) {
     // Set your email body
     if($caller == 'client'){
 
@@ -22,25 +22,24 @@ function sendConfirmationEmail($name, $email, $subject, $message,$caller) {
         <div style=\"background-color: #181C85; color: #fff; padding: 20px; text-align: center; position: relative;\">
             <h1 style=\"margin-bottom: 10px;\">MUKFAT TECHNOLOGIES SMC LTD</h1>
             <p>head office Kampala, Uganda</p>
-            <h4> ENGINEERING DEPARTMENT</h4>
+            <h4> SALES DEPARTMENT</h4>
         </div>
         <div style=\"background-color: #F04D0C; height: 5px;\"></div> <!-- Blue strip -->
         <div style=\"padding: 20px; background-color: #f2f2f2;\"> <!-- Light grey background -->
             <p>Dear $name,</p>
-            <p>Subject: <strong style=\"text-decoration: underline;\">$subject</strong> </p>
-            <p> We are writing to confirm the receipt of your booking. Your project is now in our system and is being processed by our engineering team.</p>
-            <p>For tracking purposes, your project has been assigned the following project number: 457-845-786.. This unique identifier will help us efficiently manage and monitor the progress of your construction project.</p>
-            <p>Rest assured, our experienced team of engineers is dedicated to delivering high-quality results and ensuring that your project progresses smoothly and efficiently.</p>
+            <p>Subject: <strong style=\"text-decoration: underline;\">$service</strong> </p>
+            <p> We are writing to confirm the receipt of your booking. Your Booking is now in our system and is being processed by our  team.</p>
+            <p>For tracking purposes, your project has been assigned the following project number: 457-845-786 This unique identifier will help us efficiently manage and monitor the progress of your booking</p>
+            <p>Rest assured, our experienced team is dedicated to delivering high-quality results and ensuring that your order progresses smoothly and efficiently.</p>
             <p>If you have any questions or require further assistance, please feel free to reach out to us. We are here to provide support and address any concerns you may have.</p>
-            <p>Thank you for choosing HARAX CONSULTS LTD. We are excited to collaborate with you and bring your project to fruition..</p>
+            <p>Thank you for choosing MUKFAT TECHNOLOGIES LTD. We are excited to collaborate with you and bring your order to fruition..</p>
             <br>
             <p>Best regards,</p>
-            <p>engineer</p>
-            <p>Managing director </p>
-            <p>HARAX CONSULTS LTD</p>
+            <p>Team</p>
+            <p>MUKFAT TECHNOLOGIES LTD</p>
         </div>
         <div style=\"background-color: #181C85; color: #fff; padding: 10px; text-align: center;\">
-            <p style=\"color:white;\">This is a letter sent with the system from the engineering department </p>
+            <p style=\"color:white;\">This is a letter sent with the system from the sales department </p>
         </div>
     </div>
     </div>";
@@ -52,7 +51,7 @@ function sendConfirmationEmail($name, $email, $subject, $message,$caller) {
         <div style=\"background-color: #181C85; color: #fff; padding: 20px; text-align: center; position: relative;\">
             <h1 style=\"margin-bottom: 10px;\">MUKFAT TECHNOLOGIES SMC LTD</h1>
             <p>head office Kampala, Uganda</p>
-            <h4> ENGINEERING DEPARTMENT</h4>
+            <h4> SALES DEPARTMENT</h4>
         </div>
         <div style=\"background-color: #F04D0C; height: 5px;\"></div> <!-- Blue strip -->
         <div style=\"padding: 20px; background-color: #f2f2f2;\"> <!-- Light grey background -->
@@ -60,17 +59,18 @@ function sendConfirmationEmail($name, $email, $subject, $message,$caller) {
             <p>A new booking has been received. Details are as follows:</p>
             <p><strong>Name:</strong> $name</p>
             <p><strong>Email:</strong> $email</p>
-            <p><strong>Subject:</strong> $subject</p>
+            <p><strong>Subject:</strong> $service</p>
+            
+            <p><strong>contact:</strong> $contact</p>
             <p><strong>Message:</strong> $message</p>
             <p>Please take appropriate action.</p>
             <br>
             <p>Best regards,</p>
-            <p>engineer</p>
-            <p>Managing Director</p>
-            <p>HARAX CONSULTS LTD</p>
+            <p>Team</p>
+            <p>MUKFAT TECHNOLOGIES LTD</p>
         </div>
         <div style=\"background-color: #181C85; color: #fff; padding: 10px; text-align: center;\">
-            <p style=\"color:white;\">This is a notification sent from the engineering department system.</p>
+            <p style=\"color:white;\">This is a notification sent from the SALES department system.</p>
         </div>
     </div>
 </div>";
@@ -106,7 +106,7 @@ function sendConfirmationEmail($name, $email, $subject, $message,$caller) {
     // Email content
     $mail->setFrom($gmail_user);
     $mail->addAddress($email);
-    $mail->Subject = $subject;
+    $mail->Subject = $service;
     $mail->isHTML(true);
     $mail->Body = $email_body;
 
@@ -135,7 +135,7 @@ $conn = connection();
 // Escape user inputs for security
 $name = $conn->real_escape_string($_POST['name']);
 $company = $conn->real_escape_string($_POST['company']);
-$contact = $conn->real_escape_string($_POST['contact']);
+$contact = $conn->real_escape_string($_POST['clientcontact']);
 $service = $conn->real_escape_string($_POST['service']);
 $message = $conn->real_escape_string($_POST['message']);
 $email = $conn->real_escape_string($_POST['email']);
@@ -144,7 +144,7 @@ $email = $conn->real_escape_string($_POST['email']);
 $sql = "INSERT INTO websitebooking (name, company, contact, service, message, email) VALUES ('$name', '$company', '$contact', '$service', '$message', '$email')";
 
 if ($conn->query($sql) === TRUE) {
-    echo sendConfirmationEmail($name, $email, $service, $message,'client');
+    echo sendConfirmationEmail($name, $email, $service, $message,$contact,'client');
 echo sendConfirmationEmail($name, $email, $service, $message,'comapny');
     echo "your booking has been placed please wait while we allocate you to our free agent ";
 } else {
